a65dda35e6c5204c1b5e321c6e5b4dd73523418e
dhl <lllee.dh@gmail.com>
2024-01-04 07:08:37 +0900

    add: boj-10814 진행중 [sort]

---
+++ BOJ/02_Silver/S5_10814/dhl.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int N;
+vector<int, string> V;
+
+// cpp vector sorting method
+bool compare(int a, string b){
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> N;
+
+    int id;
+    string name;
+    V = vector<int, string>(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> id >> name;
+        V.push_back(N);
+    }
+
+    return 0;
+}
