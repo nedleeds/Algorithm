51647570ff60c4bc8184cb19b61f58a1ba2f8d17
dhl <lllee.dh@gmail.com>
2024-01-09 06:33:49 +0900

    fix: boj-10845 큐STL 사용함[data structure, queue]

--- BOJ/02_Silver/S4_10845/dhl.cpp
+++ BOJ/02_Silver/S4_10845/dhl.cpp
@@ -1,113 +1,61 @@
 #include <iostream>
 #include <string>
+#include <queue>
 using namespace std;
 
-// 노드 구조체 정의
-struct Node
-{
-    int value;
-    Node* next;
-    // cpp's member initializer list (생성자 정의)
-    Node(int data) : value(data), next(nullptr) {};
-};
-
-// 큐 클래스 정의
-class Queue {
-    Node *front, *rear;
-    int size;
-public:
-    Queue(){
-        front = rear = nullptr;
-    }
-
-    void push(int value){
-        this->size++;
-        Node* newNode = new Node(value);
-        if (rear == nullptr){
-            front = rear = newNode;
-            return;
-        }
-        // before update
-        rear->next = newNode;
-        rear = newNode;
-    }
-
-    void pop(){
-        if (front == nullptr){
-            cout << "-1\n";
-            return ;
-        }
-
-        this->size--;
-        Node* temp = front;
-        front = front->next;
-        if (front == nullptr){
-            rear = nullptr;
-        }
-        delete temp;
-    }
-
-    int front(){
-        if (front == nullptr){
-            return -1;
-        }
-
-        return front->value;
-    }
-
-    int back(){
-        if (rear == nullptr){
-            return -1;
-        }
-
-        return rear->value;
-    }
-
-    bool isEmpty(){
-        if (front == nullptr && rear == nullptr){
-            return true;
-        }
-        return false;
-    }
-    int size(){
-        return this->size;
-    }
-};
+int N;
 
 int main(){
-    int n_cmd;
-    cin >> n_cmd;
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    Queue q;
-    string op;
+    cin >> N;
+    queue<int> q;
 
-    while(n_cmd--){
-        cin >> op;
-        if (op == "push") {
-            int value;
-			cin >> value;
-			q.push(value);
-		}
-		else if (op == "pop") {
-			if (q.isEmpty()) cout << -1 << '\n';
-			else {
-				cout << q.front() << '\n';
-				q.pop();
-			}
-		}
-		else if (op == "size") {
-			cout << q.size() << '\n';
-		}
-		else if (op == "empty") {
-			cout << q.isEmpty() << '\n';
-		}
-		else if (op == "front") {
-			if (q.isEmpty()) cout << -1 << '\n';
-			else cout << q.front() << '\n';
-		}
-		else if (op == "back") {
-			if (q.isEmpty()) cout << -1 << '\n';
-			else cout << q.back() << '\n';
-		}
+    while(N--){
+        string cmd;
+        cin >> cmd;
+        
+        if (cmd == "push"){
+            int v;
+            cin >> v;
+            q.push(v);
+        }
+        else if (cmd == "pop"){
+            if (q.empty()){
+                cout << "-1\n";
+            }
+            else{
+                cout << q.front() << '\n';
+                q.pop();
+            }
+        }
+        else if (cmd == "size"){
+            cout << q.size() << '\n';
+        }
+        else if (cmd == "empty"){
+            if (q.empty()){
+                cout << "1\n";
+            }
+            else{
+                cout << "0\n";
+            }
+        }
+        else if (cmd == "front"){
+            if (q.empty()){
+                cout << "-1\n";
+            }
+            else {
+                cout << q.front() << '\n';
+            }
+        }
+        else if (cmd == "back"){
+            if (q.empty()){
+                cout << "-1\n";
+            }
+            else {
+                cout << q.back() << '\n';
+            }
+        }
     }
 }
