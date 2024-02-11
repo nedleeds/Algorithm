c379675ba994fcfe6ced9b2a44ab01e14f0dc22f
dhl <lllee.dh@gmail.com>
2024-02-11 18:31:47 +0900

    fix: boj-1463 DP 첫시작[다이나믹프로그래밍]

--- BOJ/02_Silver/S3_1463/dhl.cpp
+++ BOJ/02_Silver/S3_1463/dhl.cpp
@@ -1,52 +1,10 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-int MIN_CNT = 2134567890;
-struct Item{ int value, cnt; };
+int arr[1000001];
 
-
-void bfs(int n){
-    queue<Item> Q;
-    Q.push({n, 0});
-
-    while (!Q.empty())
-    {
-        for (int i = 0; i < Q.size(); i++){
-            Item now = Q.front();
-            Q.pop();
-
-            if (now.value % 3 == 0){
-                if (now.value / 3 == 1){
-                    MIN_CNT = now.cnt + 1;
-                    return;
-                }
-                else{
-                    Q.push({now.value / 3, now.cnt +1});
-                }
-            }
-            if (now.value % 2 == 0){
-                if (now.value / 2 == 1){
-                    MIN_CNT = now.cnt + 1;
-                    return;
-                }
-                else {
-                    Q.push({now.value / 2, now.cnt + 1});
-                }
-            }
-            if (now.value - 1 == 1){
-                MIN_CNT = now.cnt + 1;
-                return;
-            }
-            else{
-                Q.push({now.value - 1, now.cnt + 1});
-            }
-        }
-    }
-}
-
-int main()
-{
+int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -55,8 +13,34 @@ int main()
     int n;
     cin >> n;
 
-    bfs(n);
-    cout << MIN_CNT << '\n';
+    arr[1] = 0;
+
+    for (int i = 2; i <= n; i++){
+        arr[i] = arr[i - 1] + 1;
+        if (i % 2 == 0) { 
+            arr[i] = min(arr[i], arr[i / 2] + 1) ; 
+        }
+        if (i % 3 == 0) { 
+            arr[i] = min(arr[i], arr[i / 3] + 1) ; 
+        }
+    }
+
+    cout << arr[n] << '\n';
 
     return 0;
 }
+
+/*
+
+3으로 나눠 떨어지면 3으로 나눈다. 
+2로 나눠 떨어지면 2로 나눈다.
+1을 뺀다.
+
+arr[i] : i 를 1로 만드는 최소 연산 수.
+arr[1] : 1 을 1로 만드는 최소 연산 수. : 0
+arr[2] : 2 를 1로 만드는 최소 연산 수. : 2 -> 1 : 1번
+arr[3] : 3 을 1로 만드는 최소 연산 수. : 3 -> 1 : 1번
+arr[4] : 4 를 1로 만드는 최소 연산 수. : 4 -> (3 or 2) -> 1 : 2번 : min(arr[3], arr[2]) + 1
+arr[5] : 5 를 1로 만드는 최소 연산 수. : 5 -> arr[4] + 1
+
+*/
