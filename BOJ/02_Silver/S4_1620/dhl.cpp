e4436583f966f647704092c2de752194ba546f0f
dhl <lllee.dh@gmail.com>
2024-02-14 06:38:45 +0900

    add: boj-1620 [자료구조, 해시]

---
+++ BOJ/02_Silver/S4_1620/dhl.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+unordered_map<string, string> Map;
+unordered_map<string, string> Map_r;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++){
+        string monster;
+        cin >> monster;
+        Map[to_string(i)] = monster;
+        Map_r[monster] = to_string(i);
+    }
+
+    for (int j = 0; j < m; j++){
+        string s;
+        cin >> s;
+
+        if ('0' <= s[0] && s[0] <= '9'){
+            cout << Map[s] << '\n';
+        }
+        else{
+            cout << Map_r[s] << '\n';
+        }
+
+    }    
+
+    return 0;
+}
