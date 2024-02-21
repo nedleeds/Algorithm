6d76fb1235cdc99e58200e6613ec790ccbfcc20b
dhl <lllee.dh@gmail.com>
2024-02-21 21:55:31 +0900

    add: boj-1931 [그리디 알고리즘, 정렬]

---
+++ BOJ/02_Silver/S1_1931/dhl.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("input.txt", "r", stdin);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        int start, end;
+        cin >> start >> end;
+
+        
+    }    
+
+    return 0;
+} 
