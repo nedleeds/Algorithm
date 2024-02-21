5f252c875b94431c2fe3570a9bffc7bd9021a5f9
dhl <lllee.dh@gmail.com>
2024-02-22 06:49:35 +0900

    fix: boj-1931 풀이 중... [그리디 알고리즘, 정렬]

--- BOJ/02_Silver/S1_1931/dhl.cpp
+++ BOJ/02_Silver/S1_1931/dhl.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
+
+struct Node { int start, end; };
+queue<int> Qstarts;
+vector<vector<int>> AdjList(100001);
+bool Visit[100001];
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -13,9 +20,21 @@ int main(){
     for (int i = 0; i < n; i++){
         int start, end;
         cin >> start >> end;
+        Qstarts.push(start);
+
+        AdjList[start].push_back(end);
+    }    
+
+    while !Qstarts.empty(){
+        int start = Qstarts.front();
+        Qstarts.pop();
+        Visit[start] =true;
+
+        if (AdjList[start].empty()){ continue; }
 
         
-    }    
+        Visit.clear();
+    }
 
     return 0;
 } 
