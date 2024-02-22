d1c8978aca2cb336f84d43e6e2a965f795cb41e3
dhl <lllee.dh@gmail.com>
2024-02-23 06:42:13 +0900

    fix: boj-1931 [그리디 알고리즘, 정렬]

--- BOJ/02_Silver/S1_1931/dhl.cpp
+++ BOJ/02_Silver/S1_1931/dhl.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-struct Node { int start, end; };
-queue<int> Qstarts;
-vector<vector<int>> AdjList(100001);
-bool Visit[100001];
+struct Node
+{
+    int start, end;
+};
+vector<Node> Nodes;
+bool compare(Node a, Node b){
+    if (a.end < b.end) {
+        return true;
+    }
+    if (a.end == b.end){
+        if (a.start < b.start) return true;
+    }
+    return false;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -18,23 +28,22 @@ int main(){
     cin >> n;
 
     for (int i = 0; i < n; i++){
-        int start, end;
-        cin >> start >> end;
-        Qstarts.push(start);
-
-        AdjList[start].push_back(end);
-    }    
-
-    while !Qstarts.empty(){
-        int start = Qstarts.front();
-        Qstarts.pop();
-        Visit[start] =true;
-
-        if (AdjList[start].empty()){ continue; }
-
-        
-        Visit.clear();
+        Node nd;
+        cin >> nd.start >> nd.end;
+        Nodes.push_back(nd);
     }
 
+    sort(Nodes.begin(), Nodes.end(), compare);
+
+    int curEnd = Nodes[0].end;
+    int cnt = 1;
+    for (int i = 1; i < Nodes.size(); i++){
+        if (Nodes[i].start < curEnd) { continue; }
+        curEnd = Nodes[i].end;
+        cnt++;
+    }
+
+    cout << cnt << '\n';
+
     return 0;
-} 
+}
