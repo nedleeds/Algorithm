8cf13f458cdef2c1728eec4902720fe090a77e10
dhl <lllee.dh@gmail.com>
2024-03-19 06:38:18 +0900

    fix: boj-2667 완전탐색[bfs, dfs, 그래프이론, 그래프탐색]

--- BOJ/02_Silver/S1_2667/dhl.cpp
+++ BOJ/02_Silver/S1_2667/dhl.cpp
@@ -1,71 +1,78 @@
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include <vector>
 #include <queue>
-#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
-int N, Cnt = 1;
-int dR[4] = {1, -1, 0, 0};
-int dC[4] = {0, 0, -1, 1};
-vector<vector<int>> Map, Vst, Grp;
-struct Node{ int r, c; };
+int N;
+int dR[4] = {-1, 1, 0, 0};
+int dC[4] = { 0, 0,-1, 1};
+vector<vector<int>> Map, Vst;
+struct Node { int r, c; };
 
-void set_map()
-{
-    cin >> N;
-    Map.resize(N, vector<int>(N, 0));
-    Vst.resize(N, vector<int>(N, 0));
-    Grp.resize(N, vector<int>(N, 0));
-
-    for (int r = 0; r < N; r++){
-        string s;
-        cin >> s;
-        for (int c = 0; c < N; c++){
-            Map[r][c] = s[c] - '0';
-        }
-    }
+bool compare(int a, int b){
+    return a < b;
 }
 
-void bfs(int r, int c)
+int bfs(Node start, int& cnt)
 {
+    int element_num = 1;
     queue<Node> q;
-    q.push({r, c});
-    Vst[r][c] = 1;
+    q.push(start);
 
     while (!q.empty()){
         Node now = q.front();
         q.pop();
 
-        if (Map[now.r][now.c]){
-            Grp[now.r][now.c] = ++Cnt;
-        }
-
         for (int i = 0; i < 4; i++){
             Node nxt = {now.r + dR[i], now.c + dC[i]};
-            if (nxt.r < 0 || nxt.r >= N) continue;
-            if (nxt.c < 0 || nxt.c >= N) continue;
-            if (Vst[nxt.r][nxt.c]) continue;
-
-            if (Map[nxt.r][nxt.c]){
-                Vst[nxt.r][nxt.c] = 1;
-                q.push(nxt);
-                Grp[nxt.r][nxt.c] = Cnt;
-            }
+            if (N <= nxt.r || nxt.r < 0 || N <= nxt.c || nxt.c < 0) continue;
+            if (!(Map[nxt.r][nxt.c]) || Vst[nxt.r][nxt.c]) continue;
+            q.push(nxt);
+            Vst[nxt.r][nxt.c] = cnt;
+            element_num++;
         }
-
     }
+
+    cnt++;
+    return element_num;
 }
 
 int main()
-{   
-    fastio;
-    freopen("input.txt", "r", stdin);
-    
-    set_map();
-    bfs(0, 0);
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    cout << Cnt << '\n';
+    freopen("input.txt", "r", stdin);
+
+    cin >> N;
+    Map.resize(N, vector<int>(N, 0));
+    Vst.resize(N, vector<int>(N, 0));
+
+    for (int r = 0; r < N; r++){
+        for (int c = 0; c < N; c++){
+            char x;
+            cin >> x;
+            Map[r][c] = x - '0';
+        }
+    }
+
+    int cnt = 1;
+    vector<int> ans;
+    for (int r = 0; r < N; r++){
+        for (int c = 0; c < N; c++){
+            if (Map[r][c] && !Vst[r][c]){
+                Vst[r][c] = cnt;
+                ans.push_back(bfs({r, c}, cnt));
+            }
+        }
+    }
+    
+    cout << cnt - 1 << '\n';
+    sort(ans.begin(), ans.end(), compare);
+    for (auto v: ans){
+        cout << v << '\n';
+    }
 
     return 0;
 }
