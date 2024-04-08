0811bec54a0f41e268cef9af1bdc8f0a9b2bb259
dhl <lllee.dh@gmail.com>
2024-04-09 06:36:18 +0900

    fix: boj-7569 [그래프 이론, 그래프 탐색, BFS]

--- BOJ/03_Gold/G5_7569/dhl.cpp
+++ BOJ/03_Gold/G5_7569/dhl.cpp
@@ -8,7 +8,7 @@ int dC[6] = {0,  0, 1, -1, 0,  0};
 int dR[6] = {0,  0, 0,  0, 1, -1};
             
 int M, N, H;
-struct Tomato{int h, r, c; };
+struct Tomato{int h, r, c, day; };
 queue<Tomato> Ripe;
 vector<vector<vector<int>>> Box;
 vector<vector<vector<int>>> Vst;
@@ -32,20 +32,19 @@ int main()
 
                 if (Box[h][r][c] == 1){
                     Vst[h][r][c] = 0;
-                    Ripe.push({h, r, c});
+                    Ripe.push({h, r, c, 0});
                 }
             }
         }
     }
 
-    int day = -1;
+    int ans = 0;
     while (!Ripe.empty()){
         Tomato now = Ripe.front();
         Ripe.pop();
-        // ++day;
 
         for (int i = 0; i < 6; i++){
-            Tomato nxt = {now.h + dH[i], now.r + dR[i], now.c + dC[i]};
+            Tomato nxt = {now.h + dH[i], now.r + dR[i], now.c + dC[i], now.day + 1};
             if (nxt.h < 0 || nxt.h >= H) continue;
             if (nxt.r < 0 || nxt.r >= N) continue;
             if (nxt.c < 0 || nxt.c >= M) continue;
@@ -53,11 +52,23 @@ int main()
             if (Vst[nxt.h][nxt.r][nxt.c] != -1) continue;
             
             Ripe.push(nxt);
-            Vst[nxt.h][nxt.r][nxt.c] = day;
+            Vst[nxt.h][nxt.r][nxt.c] = nxt.day;
+            ans = nxt.day;
         }
     }
 
-    cout << day << '\n';
+    for (int h = 0; h < H; h++){
+        for (int r = 0; r < N; r++){
+            for (int c = 0; c < M; c++){
+                if (Box[h][r][c] == 0 && Vst[h][r][c] == -1){
+                    cout << -1 << '\n';
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
